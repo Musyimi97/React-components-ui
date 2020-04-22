419276981b216209a59510e7d6fab2e34a139b8d
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-23 01:07:53 +0300

    completed forms

--- src/components/formuserdetails.js
+++ src/components/formuserdetails.js
@@ -5,13 +5,56 @@ import TextField from 'material-ui/TextField';
 import RaisedButtton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
+    continue = e =>{
+        e.preventDefault();
+        this.props.nextStep();
+
+    }
     render() {
+        const { values, handleChange } =this.props;
         return (
-            <div>
-                <h1>Form User details</h1>
-            </div>
+            <MuiThemeProvider>
+                <React.Fragment>
+                <AppBar title="Enter User Details" />
+                <TextField 
+                    hintStyle="Enter your Firts Name"
+                    floatingLabelText="First Name"
+                    onChange={handleChange ('firstName')}
+                    defaultValue = {values.firstName}
+                />
+                <br/>
+
+                <TextField 
+                    hintStyle="Enter your Last Name"
+                    floatingLabelText="Last Name"
+                    onChange={handleChange ('lastName')}
+                    defaultValue = {values.lastName}
+                />
+                <br/>
+
+                <TextField 
+                    hintStyle="Enter your email"
+                    floatingLabelText="email"
+                    onChange={handleChange ('email')}
+                    defaultValue = {values.email}
+                />
+                <br/>
+                <RaisedButtton
+                    label="Continue"
+                    primary = {true}
+                    style = {styles.button}
+                    onClick={this.continue}
+                />
+
+                </React.Fragment>
+            </MuiThemeProvider>
         )
     }
 }
 
+const styles = {
+    button:{
+        margin:15
+    }
+}
 export default FormUserDetails
