7c6b3c36fda67895b778f681cbae15e0c849f9c5
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-23 01:36:43 +0300

    personal etails done and completed

--- src/components/userforms.js
+++ src/components/userforms.js
@@ -1,5 +1,7 @@
+/* eslint-disable default-case */
 import React, { Component } from 'react'
 import FormUserDetails from './formuserdetails'
+import FormPersonalDetails from './formpersoanldetails'
 
 export class Userforms extends Component {
     state={
@@ -52,7 +54,15 @@ export class Userforms extends Component {
                     />
                 )
                 case 2:
-                    return <h1>FormPersonalDetails</h1>
+                    return(
+                        <FormPersonalDetails 
+                            nextStep={this.nextStep}
+                            prevStep={this.prevStep}
+
+                            handleChange={this.handleChange}
+                            values = {values}
+                        />
+                    )
 
                 case 3:
                     return <h1>Confirm</h1>
